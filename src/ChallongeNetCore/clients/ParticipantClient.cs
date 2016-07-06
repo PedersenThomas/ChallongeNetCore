bf4fcdf8c9c642e50584995ba1d1e5f6a78f27b3
Thomas Pedersen <pedersenthomas@outlook.com>
2016-07-06 13:24:31 +0200

    Added ParticipantClient with indexRequest

---
+++ src/ChallongeNetCore/clients/ParticipantClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChallongeNetCore.clients
+{
+    public class ParticipantClient
+    {
+        public ChallongeV1Connection connection { get; private set; }
+
+        public ParticipantClient(ChallongeV1Connection connection)
+        {
+            this.connection = connection;
+        }
+
+        /// <summary>
+        /// Retrieve a tournament's participant list.
+        /// </summary>
+        /// <returns></returns>
+        public clients.ParticipantRequest.IndexRequest IndexRequest()
+        {
+            return new clients.ParticipantRequest.IndexRequest(this.connection);
+        }
+    }
+}
---
+++ src/ChallongeNetCore/clients/ParticipantRequest/IndexRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChallongeNetCore.clients.ParticipantRequest
+{
+    public class IndexRequest
+    {
+        private IDictionary<string, dynamic> parameters = new Dictionary<string, dynamic>();
+
+        public ChallongeV1Connection connection { get; private set; }
+
+        public IndexRequest(ChallongeV1Connection connection)
+        {
+            this.connection = connection;
+        }
+
+
+        public async Task<IList<Tournament>> SendAsync(string TournamentIdentifier)
+        {
+            string apiUrl = $"/tournaments/{TournamentIdentifier}/participants";
+            const string method = "GET";
+            var jsonString = await connection.MakeJSONRequestAsync(apiUrl, method, parameters);
+            return Deserializer.ListOfTournaments(jsonString);
+        }
+    }
+}
