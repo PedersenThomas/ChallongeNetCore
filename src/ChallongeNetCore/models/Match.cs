467b51e372dfeae123500da9e65316127de91270
Thomas Pedersen <pedersenthomas@outlook.com>
2019-10-02 20:17:40 +0200

    Converted the Library to use System.Text.Json instead of Newtonsoft

--- src/ChallongeNetCore/models/Match.cs
+++ src/ChallongeNetCore/models/Match.cs
@@ -1,6 +1,5 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using System;
+﻿using System;
+using System.Text.Json.Serialization;
 
 namespace ChallongeNetCore
 {
@@ -13,70 +12,70 @@ namespace ChallongeNetCore
 
     public class Match
     {
-        [JsonProperty(ItemConverterType = typeof(IsoDateTimeConverter), PropertyName = "created_at")]
+        [JsonPropertyName("created_at")]
         public DateTime? CreatedAt { get; set; }
 
-        [JsonProperty(PropertyName = "group_id")]
+        [JsonPropertyName("group_id")]
         public int? GroupId { get; set; }
 
-        [JsonProperty(PropertyName = "has_attachment")]
+        [JsonPropertyName("has_attachment")]
         public bool HasAttachment { get; set; }
 
-        [JsonProperty(PropertyName = "id")]
+        [JsonPropertyName("id")]
         public int Id { get; set; }
 
-        [JsonProperty(PropertyName = "identifier")]
+        [JsonPropertyName("identifier")]
         public string Identifier { get; set; }
 
-        [JsonProperty(PropertyName = "loser_id")]
+        [JsonPropertyName("loser_id")]
         public int? LoserId { get; set; }
 
-        [JsonProperty(PropertyName = "player1_id")]
+        [JsonPropertyName("player1_id")]
         public int? Player1Id { get; set; }
 
-        [JsonProperty(PropertyName = "player1_is_prereq_match_loser")]
+        [JsonPropertyName("player1_is_prereq_match_loser")]
         public bool Player1IsPrereqMatchLoser { get; set; }
 
-        [JsonProperty(PropertyName = "player1_prereq_match_id")]
+        [JsonPropertyName("player1_prereq_match_id")]
         public int? Player1PrereqMatchId { get; set; }
 
-        [JsonProperty(PropertyName = "player1_votes")]
+        [JsonPropertyName("player1_votes")]
         public string Player1Votes { get; set; }
 
-        [JsonProperty(PropertyName = "player2_id")]
+        [JsonPropertyName("player2_id")]
         public int? Player2Id { get; set; }
 
-        [JsonProperty(PropertyName = "player2_is_prereq_match_loser")]
+        [JsonPropertyName("player2_is_prereq_match_loser")]
         public bool Player2IsPrereqMatchLoser { get; set; }
 
-        [JsonProperty(PropertyName = "player2_prereq_match_id")]
+        [JsonPropertyName("player2_prereq_match_id")]
         public int? Player2PrereqMatchId { get; set; }
 
-        [JsonProperty(PropertyName = "player2_votes")]
+        [JsonPropertyName("player2_votes")]
         public string Player2Votes { get; set; }
 
-        [JsonProperty(PropertyName = "round")]
+        [JsonPropertyName("round")]
         public int Round { get; set; }
 
-        [JsonProperty(ItemConverterType = typeof(IsoDateTimeConverter), PropertyName = "started_at")]
+        [JsonPropertyName("started_at")]
         public DateTime? StartedAt { get; set; }
 
-        [JsonProperty(ItemConverterType = typeof(StringEnumConverter), PropertyName = "state")]
+        [JsonPropertyName("state")]
         public MatchState State { get; set; }
 
-        [JsonProperty(PropertyName = "tournament_id")]
+        [JsonPropertyName("tournament_id")]
         public int TournamentId { get; set; }
 
-        [JsonProperty(ItemConverterType = typeof(IsoDateTimeConverter), PropertyName = "updated_at")]
+        [JsonPropertyName("updated_at")]
         public DateTime? UpdatedAt { get; set; }
 
-        [JsonProperty(PropertyName = "winner_id")]
+        [JsonPropertyName("winner_id")]
         public int? WinnerId { get; set; }
 
-        [JsonProperty(PropertyName = "prerequisite_match_ids_csv")]
+        [JsonPropertyName("prerequisite_match_ids_csv")]
         public string PrerequisiteMatchIdsCsv { get; set; }
 
-        [JsonProperty(PropertyName = "scores_csv")]
+        [JsonPropertyName("scores_csv")]
         public string ScoresCsv { get; set; }
     }
 }
--- src/ChallongeNetCore/models/tournament.cs
+++ src/ChallongeNetCore/models/tournament.cs
@@ -1,199 +1,198 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace ChallongeNetCore
 {
     public class Tournament
     {
-        [JsonProperty(PropertyName = "accept_attachments")]
+        [JsonPropertyName("accept_attachments")]
         public bool AcceptAttachments { get; set; }
 
-        [JsonProperty(PropertyName = "allow_participant_match_reporting")]
+        [JsonPropertyName("allow_participant_match_reporting")]
         public bool AllowParticipantMatchReporting { get; set; }
 
-        [JsonProperty(PropertyName = "anonymous_voting")]
+        [JsonPropertyName("anonymous_voting")]
         public bool AnonymousVoting { get; set; }
 
-        [JsonProperty(PropertyName = "category")]
+        [JsonPropertyName("category")]
         public string Category { get; set; }
 
-        [JsonProperty(PropertyName = "check_in_duration")]
+        [JsonPropertyName("check_in_duration")]
         public int? CheckInDuration { get; set; }
 
-        [JsonProperty(ItemConverterType = typeof(IsoDateTimeConverter), PropertyName = "completed_at")]
+        [JsonPropertyName("completed_at")]
         public DateTime? CompletedAt { get; set; }
 
-        [JsonProperty(ItemConverterType = typeof(IsoDateTimeConverter), PropertyName = "created_at")]
+        [JsonPropertyName("created_at")]
         public DateTime? CreatedAt { get; set; }
 
-        [JsonProperty(PropertyName = "created_by_api")]
+        [JsonPropertyName("created_by_api")]
         public bool CreatedByApi { get; set; }
 
-        [JsonProperty(PropertyName = "credit_capped")]
+        [JsonPropertyName("credit_capped")]
         public bool CreditCapped { get; set; }
 
-        [JsonProperty(PropertyName = "description")]
+        [JsonPropertyName("description")]
         public string Description { get; set; }
 
-        [JsonProperty(PropertyName = "game_id")]
+        [JsonPropertyName("game_id")]
         public int? GameId { get; set; }
 
-        [JsonProperty(PropertyName = "enable_group_stage")]
+        [JsonPropertyName("enable_group_stage")]
         public bool EnableGroupStage { get; set; }
 
-        [JsonProperty(PropertyName = "hide_forum")]
+        [JsonPropertyName("hide_forum")]
         public bool HideForum { get; set; }
 
-        [JsonProperty(PropertyName = "hide_seeds")]
+        [JsonPropertyName("hide_seeds")]
         public bool HideSeeds { get; set; }
 
-        [JsonProperty(PropertyName = "hold_third_place_match")]
+        [JsonPropertyName("hold_third_place_match")]
         public bool HoldThirdPlaceMatch { get; set; }
 
-        [JsonProperty(PropertyName = "id")]
+        [JsonPropertyName("id")]
         public int Id { get; set; }
 
-        [JsonProperty(PropertyName = "matches")]
+        [JsonPropertyName("matches")]
         public List<WrapperMatch> Matches { get; set; }
 
-        [JsonProperty(PropertyName = "max_predictions_per_user")]
+        [JsonPropertyName("max_predictions_per_user")]
         public int MaxPredictionsPerUser { get; set; }
 
-        [JsonProperty(PropertyName = "name")]
+        [JsonPropertyName("name")]
         public string Name { get; set; }
 
-        [JsonProperty(PropertyName = "notify_users_when_matches_open")]
+        [JsonPropertyName("notify_users_when_matches_open")]
         public bool NotifyUsersWhenMatchesOpen { get; set; }
 
-        [JsonProperty(PropertyName = "notify_users_when_the_tournament_ends")]
+        [JsonPropertyName("notify_users_when_the_tournament_ends")]
         public bool NotifyUsersWhenTheTournamentEnds { get; set; }
 
-        [JsonProperty(PropertyName = "open_signup")]
+        [JsonPropertyName("open_signup")]
         public bool OpenSignup { get; set; }
 
-        [JsonProperty(PropertyName = "participants_count")]
+        [JsonPropertyName("participants_count")]
         public int ParticipantsCount { get; set; }
 
-        [JsonProperty(PropertyName = "prediction_method")]
+        [JsonPropertyName("prediction_method")]
         public int PredictionMethod { get; set; }
 
-        [JsonProperty(ItemConverterType = typeof(IsoDateTimeConverter), PropertyName = "predictions_opened_at")]
+        [JsonPropertyName("predictions_opened_at")]
         public DateTime? PredictionsOpenedAt { get; set; }
 
-        [JsonProperty(PropertyName = "private")]
+        [JsonPropertyName("private")]
         public bool IsPrivate { get; set; }
 
-        [JsonProperty(PropertyName = "participants")]
+        [JsonPropertyName("participants")]
         public List<WrapperParticipant> Participants { get; set; }
 
-        [JsonProperty(PropertyName = "progress_meter")]
+        [JsonPropertyName("progress_meter")]
         public int ProgressMeter { get; set; }
 
-        [JsonProperty(PropertyName = "pts_for_bye")]
+        [JsonPropertyName("pts_for_bye")]
         public float PtsForBye { get; set; }
 
-        [JsonProperty(PropertyName = "pts_for_game_tie")]
+        [JsonPropertyName("pts_for_game_tie")]
         public float PtsForGameTie { get; set; }
 
-        [JsonProperty(PropertyName = "pts_for_game_win")]
+        [JsonPropertyName("pts_for_game_win")]
         public float PtsForGameWin { get; set; }
 
-        [JsonProperty(PropertyName = "pts_for_match_tie")]
+        [JsonPropertyName("pts_for_match_tie")]
         public float PtsForMatchTie { get; set; }
 
-        [JsonProperty(PropertyName = "pts_for_match_win")]
+        [JsonPropertyName("pts_for_match_win")]
         public float PtsForMatchWin { get; set; }
 
-        [JsonProperty(PropertyName = "quick_advance")]
+        [JsonPropertyName("quick_advance")]
         public bool QuickAdvance { get; set; }
 
-        [JsonProperty(PropertyName = "ranked_by")]
+        [JsonPropertyName("ranked_by")]
         public TournamentRankedBy? RankedBy { get; set; }
 
-        [JsonProperty(PropertyName = "require_score_agreement")]
+        [JsonPropertyName("require_score_agreement")]
         public bool RequireScoreAgreement { get; set; }
 
-        [JsonProperty(PropertyName = "rr_pts_for_game_tie")]
+        [JsonPropertyName("rr_pts_for_game_tie")]
         public float RrPtsForGameTie { get; set; }
 
-        [JsonProperty(PropertyName = "rr_pts_for_game_win")]
+        [JsonPropertyName("rr_pts_for_game_win")]
         public float RrPtsForGameWin { get; set; }
 
-        [JsonProperty(PropertyName = "rr_pts_for_match_tie")]
+        [JsonPropertyName("rr_pts_for_match_tie")]
         public float RrPtsForMatchTie { get; set; }
 
-        [JsonProperty(PropertyName = "rr_pts_for_match_win")]
+        [JsonPropertyName("rr_pts_for_match_win")]
         public float RrPtsForMatchWin { get; set; }
 
-        [JsonProperty(PropertyName = "sequential_pairings")]
+        [JsonPropertyName("sequential_pairings")]
         public bool SequentialPairings { get; set; }
 
-        [JsonProperty(PropertyName = "show_rounds")]
+        [JsonPropertyName("show_rounds")]
         public bool ShowRounds { get; set; }
 
-        [JsonProperty(PropertyName = "signup_cap")]
+        [JsonPropertyName("signup_cap")]
         public int? SignupCap { get; set; }
 
-        [JsonProperty(ItemConverterType = typeof(IsoDateTimeConverter), PropertyName = "start_at")]
+        [JsonPropertyName("start_at")]
         public DateTime? StartAt { get; set; }
 
-        [JsonProperty(ItemConverterType = typeof(IsoDateTimeConverter), PropertyName = "started_at")]
+        [JsonPropertyName("started_at")]
         public DateTime? StartedAt { get; set; }
 
-        [JsonProperty(ItemConverterType = typeof(IsoDateTimeConverter), PropertyName = "started_checking_in_at")]
+        [JsonPropertyName("started_checking_in_at")]
         public DateTime? StartedCheckingInAt { get; set; }
 
-        [JsonProperty(PropertyName = "state")]
+        [JsonPropertyName("state")]
         public TournamentState Tournamentstate { get; set; }
 
-        [JsonProperty(PropertyName = "swiss_rounds")]
+        [JsonPropertyName("swiss_rounds")]
         public int SwissRounds { get; set; }
 
-        [JsonProperty(ItemConverterType = typeof(StringEnumConverter), PropertyName = "tournament_type")]
+        [JsonPropertyName("tournament_type")]
         public TournamentType TournamentType { get; set; }
 
-        [JsonProperty(ItemConverterType = typeof(IsoDateTimeConverter), PropertyName = "updated_at")]
+        [JsonPropertyName("updated_at")]
         public DateTime? UpdatedAt { get; set; }
 
-        [JsonProperty(PropertyName = "url")]
+        [JsonPropertyName("url")]
         public string Url { get; set; }
 
-        [JsonProperty(PropertyName = "description_source")]
+        [JsonPropertyName("description_source")]
         public string DescriptionSource { get; set; }
 
-        [JsonProperty(PropertyName = "subdomain")]
+        [JsonPropertyName("subdomain")]
         public string Subdomain { get; set; }
 
-        [JsonProperty(PropertyName = "full_challonge_url")]
+        [JsonPropertyName("full_challonge_url")]
         public string FullChallongeUrl { get; set; }
 
-        [JsonProperty(PropertyName = "live_image_url")]
+        [JsonPropertyName("live_image_url")]
         public string LiveImageUrl { get; set; }
 
-        [JsonProperty(PropertyName = "sign_up_url")]
+        [JsonPropertyName("sign_up_url")]
         public string SignUpUrl { get; set; }
 
-        [JsonProperty(PropertyName = "review_before_finalizing")]
+        [JsonPropertyName("review_before_finalizing")]
         public bool ReviewBeforeFinalizing { get; set; }
 
-        [JsonProperty(PropertyName = "accepting_predictions")]
+        [JsonPropertyName("accepting_predictions")]
         public bool AcceptingPredictions { get; set; }
 
-        [JsonProperty(PropertyName = "participants_locked")]
+        [JsonPropertyName("participants_locked")]
         public bool ParticipantsLocked { get; set; }
 
-        [JsonProperty(PropertyName = "game_name")]
+        [JsonPropertyName("game_name")]
         public string GameName { get; set; }
 
-        [JsonProperty(PropertyName = "participants_swappable")]
+        [JsonPropertyName("participants_swappable")]
         public bool ParticipantsSwappable { get; set; }
 
-        [JsonProperty(PropertyName = "team_convertable")]
+        [JsonPropertyName("team_convertable")]
         public bool TeamConvertable { get; set; }
 
-        [JsonProperty(PropertyName = "group_stages_were_started")]
+        [JsonPropertyName("group_stages_were_started")]
         public bool GroupStagesWereStarted { get; set; }
     }
 }
